1269a0a859ae627e619cf6e199abab08c806b95f
Ian Owira <ian@owira.co.za>
2022-09-02 16:05:05 +0200

    - code refactoring - form partials - before_action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,11 @@
 class PostsController < ApplicationController
+  before_action :get_post, only: [:show, :edit, :update, :destroy]
+
   def index
     @posts = Post.all
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
@@ -12,11 +13,10 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def create
-    @post = Post.new(params.require(:post).permit(:title, :description))
+    @post = Post.new(post_params)
     
     if @post.save
       flash[:notice] = "Post #{@post.title} was created successfully!"
@@ -27,8 +27,7 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post = Post.find(params[:id])
-    if @post.update(params.require(:post).permit(:title, :description))
+    if @post.update(post_params)
       flash[:notice] = "Post was updated successfully!"
       redirect_to @post
     else
@@ -37,9 +36,18 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
 
     redirect_to posts_path
+  end
+
+  private
+
+  def get_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :description)
   end
 end
