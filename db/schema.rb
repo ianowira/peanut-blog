91ed310aea477a6c23d3f4a9f7fc3f3e3296a6b3
Ian Owira <ian@owira.co.za>
2022-11-24 19:41:14 +0200

    added user

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_19_191127) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_24_173506) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
