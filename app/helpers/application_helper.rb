c2a74c39de1471237f25d7f152bac81a0053c72e
Ian Owira <ian@owira.co.za>
2023-01-12 17:05:32 +0200

    - profile picture - associated posts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def gravatar_for(user, size = 80)
+    email_address = user.email.downcase
+    hash = Digest::MD5.hexdigest(email_address)
+    image_src = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+    image_tag(image_src, alt: @user.username, class: "rounded mx-auto d-block")
+  end
 end
