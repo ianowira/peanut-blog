990d6bbc900ab6b3e584190284a3ff574407362a
Ian Owira <ian@owira.co.za>
2023-01-16 22:33:25 +0200

    added category model and tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_16_180043) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_16_200616) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "description"
